6bf9d8528823a54755ef485860d5a6e564031b1c
Ли Хайцюань <yamahatvt@gmail.com>
2026-04-18 20:08:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tracks = [
-    { title: "Трек 1 (Без авторских прав)", src: "music/track1.mp3" },
+    { title: "LSP Sicrit Place", src: "music/LSP_-_Sikret_Plejjs_79396301.mp3" },
     { title: "Трек 2 (Лицензионный)", src: "music/track2.mp3" }
 ];
 
